7181640579c2d238c97f4ef2c1015506310c769c
Philip Kertsman <pkerts@uw.edu>
2018-10-31 14:52:24 -0700

    Reading the travel times correctly works. Now I am just rerouting the data - after I've read it - to another text and confirming they're the same. Currently now it is time to store the values(/data) in memory then print to confirm not only that the data through and through is being negotiated correctly but that it can be saved in memory. Thanks!

--- Ass3/main.cpp
+++ Ass3/main.cpp
@@ -2,14 +2,57 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-
+#include <vector>
 
 using namespace std;
+
+struct Data {
+	string city;
+	string state;
+	string zipCode;
+};
+
+istream& operator>>(istream& is, Data& d) {
+	string origin;
+	string dest;
+	string time;
+	// int travel_time;
+	if (getline(is, origin, '\t') &&
+		getline(is, dest, '\t') &&
+		getline(is, time)) {
+		// d.zipCode = stoi(zipcode);
+		cout << origin << '\t' << dest << '\t' << time << '\n';
+	}
+
+	return is;
+}
 
 int main(int argc, char** argv) {
 	string filename = argv[1];
 	fstream in(filename);
 
-	cout << "hello" << endl;
+	Data d;
+	// string size;
+	// getline(in, size);
+	// getline(in, size);
+
+	while (in >> d) {
+		// cout << "Yes! It worked!";
+		;
+	}
+
+	// cout << "\n" << d.city << " " << d.state << " " << d.zipCode << endl;
+
+	/*cout << "hello" << endl;
+
+	string line;
+	while (getline(in, line)) {
+		istringstream iss(line);
+		string a, b, c;
+		int d, e;
+		iss >> a >> b >> d >> c >> e;
+		cout << a << " " << b << " " << d << " " << c << " " << e << endl;
+	}*/
+
 	return 0;
 }
