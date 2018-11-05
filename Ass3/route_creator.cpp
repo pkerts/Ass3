1dcf3208828759dcd8f5d4c1843cc9f535cb5282
Philip Kertsman <pkerts@uw.edu>
2018-11-04 23:31:32 -0800

    Created route creator declaration

---
+++ Ass3/route_creator.cpp
@@ -0,0 +1,8 @@
+﻿#include "route_creator.h"
+#include <iostream>
+
+using namespace std;
+
+void RouteCreator::Create() {
+	cout << "Enter ship name" << endl;
+}
---
+++ Ass3/route_creator.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+class RouteCreator {
+public:
+	void Create();
+};
