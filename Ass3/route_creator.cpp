462653349014057e006ff238860988f2e8f13069
Philip Kertsman <pkerts@uw.edu>
2018-11-05 05:30:07 -0800

    fghdf

--- Ass3/route_creator.cpp
+++ Ass3/route_creator.cpp
@@ -9,14 +9,16 @@ using namespace std;
 void RouteCreator::Create(const Reader& r, const Travel_Times& t) {
 	ofstream route("route.txt");
 	int status = 111;
-	auto time = 0;
+	int time = 0;
+	int ship = 0;
+	int origin = 0;
 	while (status != 0) {
-		int ship;
-		cout << "Ships:" << endl;
-		r.dump_ships();
-		cout << "Select ship" << endl;
-		cin >> ship;
-		switch (ship) {
+		if (status != 2) {
+			cout << "Ships:" << endl;
+			r.dump_ships();
+			cout << "Select ship" << endl;
+			cin >> ship;
+			switch (ship) {
 			case 1: route << "Andromeda Ascendant\t"; break;
 			case 2: route << "USSC Discovery One\t"; break;
 			case 3: route << "Galactica\t"; break;
@@ -27,16 +29,18 @@ void RouteCreator::Create(const Reader& r, const Travel_Times& t) {
 			case 8: route << "Red Dwarf\t"; break;
 			case 9: route << "Serenity\t"; break;
 			case 10: route << "USS Enterprise\t"; break;
-		default: ;
+			default:;
+			}
+
+			cout << "\nSelect origin\nPlanets:" << endl;
+			t.dump(0);
+			cin >> origin;
+			cout << endl;
+			auto origin_name = t.dump(origin);
+			route << origin_name << '\t' << time << '\t';
 		}
-		cout << "\nSelect origin" << endl;
-		int origin = 0;
-		cout << "Planets:" << endl;
-		t.dump(0);
-		cin >> origin;
-		cout << endl;
-		auto origin_name = t.dump(origin);
-		route << origin_name << '\t' << time << '\t';
+		
+		
 		cout << "Select destination" << endl;
 		int destination = 0;
 		cin >> destination;
